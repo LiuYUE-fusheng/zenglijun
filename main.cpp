56d3d1742b1bea5928093d538a0f524ccc449663
LiuYUE-fusheng <62330146+LiuYUE-fusheng@users.noreply.github.com>
2020-04-02 12:55:32 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,37 @@
 #include<iostream>
-#include <bits/stdc++.h>
+#include <stdio.h>
+#include <stdlib.h>
 using namespace std;
 
 int main()
 {
-	int arr[10],N;
-	int proSum=0,maxSum= 0;
-	scanf("%d",&N);
-	for(int k=0;k<N;k++)
-    {
-        scanf("%d",&arr[k]);
-    }
+	int arr[10], N;
+	int proSum = 0, maxSum = 0;
+	cin >> N;
+	for (int k = 0; k < N; k++)
+	{
+		cin >> arr[k];
+	}
 
-	for(int i=0;i<N;i++)
+	for (int i = 0; i < N; i++)
 	{
 
-        if(proSum<0)
+		if (proSum < 0)
 			proSum = arr[i];
 		else
-			proSum = proSum+arr[i];
-		if(maxSum < proSum)
+			proSum = proSum + arr[i];
+		if (maxSum < proSum)
 			maxSum = proSum;
-			printf("以%d为右边界的子段和最大值是%d\n",arr[i],maxSum);
+		cout << "以" << arr[i] << "为右边界的子段和最大和为：" << maxSum << endl;
 	}
-	if(N<0)
-    {
-        printf("该序列最大值为0");
-    }
-    else
-    {
-        printf("该序列最大值为%d\n",maxSum);
-    }
+	if (N < 0)
+	{
+		cout << "该序列最大值为0" << endl;
+	}
+	else
+	{
+		cout << "该序列最大值为" << maxSum << endl;
+	}
 
 	return 0;
 }
